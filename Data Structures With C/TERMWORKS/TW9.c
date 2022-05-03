a8056ef149fadb5c357296f1b932b0c322d9ab11
= <=>
2022-05-03 17:43:58 +0530

    Changes to be committed: 	modified:   Data Structures With C/TERMWORKS/TW9.c 	new file:   Practice/TW9.c

--- Data Structures With C/TERMWORKS/TW9.c
+++ Data Structures With C/TERMWORKS/TW9.c
@@ -1,136 +1,126 @@
-// TW 9 Expression Tree
+//Expression tree
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
+#include<ctype.h>
 
-
-struct node {
+typedef struct node
+{
     char data;
-    struct node* left;
-    struct node* right;
-    struct node* next;
-};
- struct node *head=NULL;
+    struct node *left;
+    struct node *right;
+    struct node *next;
+}Node;
 
-struct node* newNode(char data)
+Node *top = NULL;
+
+Node* newNode(char);
+void inorder(Node*);
+void preorder(Node*);
+void postorder(Node*);
+void push(Node*);
+Node* pop();
+
+Node* newNode(char data)
 {
-    struct node* node = (struct node*)malloc(sizeof(struct node));
-    node->data = data;
-    node->left = NULL;
-    node->right = NULL;
-    node->next = NULL;
-
-    return (node);
+    Node* root = (Node*)malloc(sizeof(Node));
+    root->data = data;
+    root->right = NULL;
+    root->left = NULL;
+    root->next = NULL;
+    return root;
 }
 
-bool isOperator(char c)
+void inorder(Node* root)
 {
-    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
-}
-
-void inorder(struct node* node)
-{
-    if(node == NULL)
-        return;
-    if (isOperator(node->data))
+    if(isalnum(root->data))
+        printf("%c", root->data);
+    else if(root != NULL)
     {
-        printf("(");
-    }
-    inorder(node->left);
-    printf("%c", node->data);
-    inorder(node->right);
-
-    if (isOperator(node->data))
-    {
-        printf(")");
+        printf("( ");
+        inorder(root->left);
+        printf(" %c ", root->data);
+        inorder(root->right);
+        printf(" )");
     }
 }
-void postorder(struct node *root)
+
+void preorder(Node* root)
 {
- if(root!=NULL)
- {
-  postorder(root->left);
-  postorder(root->right);
-  printf("%c ",root->data);
- }
+    if(root != NULL)
+    {
+        printf("%c ",root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
 }
 
-void preorder(struct node *root)
+void postorder(Node* root)
 {
- if(root!=NULL)
- {
-  printf("%c ",root->data);
-  preorder(root->left);
-  preorder(root->right);
- }
+    if(root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%c ",root->data);
+    }
 }
 
-
-void push(struct node* x)
+void push(Node* root)
 {
-    if(head==NULL)
-    head = x;
+    if(top == NULL)
+        root->next = NULL;
     else
-    {
-        (x)->next = head;
-        head  = x;
-    }
+        root->next = top;
+    top = root;
 }
 
-struct node* pop()
+Node* pop()
 {
-    struct node* p = head;
-    head = head->next;
-    return p;
+    if(top == NULL)
+        return NULL;
+    Node* newNode = top;
+    top = top->next;
+    return newNode;
 }
 
-int main()
+int main(void)
 {
     char s[30];
-    printf("\nEnter the expression: ");
+    Node *root;
+    int i,l;
+    
+    printf("\nEnter the postfix expression: ");
     scanf("%[^\n]s", s);
-    int l = strlen(s);
-    for(int i=0;i<l;i++)
+    l = strlen(s);
+
+    printf("\nExpression: ");
+    for(i=0;i<l;i++)
         printf("%c ", s[i]);
-    printf("\nl = %d\n", l);
-    struct node *x, *y, *z;
-    for (int i = 0; i < l; i++) {
- 
-        if (s[i] == '+' || s[i] == '-' || s[i] == '*'
-            || s[i] == '/')
-        {
-            z = newNode(s[i]);
-            x = pop();
-            y = pop();
-            z->left = y;
-            z->right = x;
-            push(z);
-        }
-        else
-        {
-            z = newNode(s[i]);
-            push(z);
-        }
-    }
-
-    printf("The given expression is\n");
-    for (int i = 0; i < l; i++)
+    printf("\nlength: %d", l);
+    
+    for(i=0;i<l;i++)
     {
-        printf("%c",s[i]);
+        root = newNode(s[i]);
+        
+        if(s[i]=='+' || s[i]=='/' || s[i]=='-' || s[i]=='*')
+        {
+            root->right = pop();
+            root->left = pop();
+        }
+        push(root);
     }
-
-    printf("\nThe Inorder Traversal of Expression Tree:\t");
-    inorder(z);
-
-    printf("\nThe Preorder Traversal of Expression Tree:\t");
-    preorder(z);
-
-    printf("\nThe Postorder Traversal of Expression Tree:\t");
-    postorder(z);
+    
+    printf("\nInorder: ");
+    inorder(root);
+    
+    printf("\nPreorder: ");
+    preorder(root);
+    
+    printf("\nPostorder: ");
+    postorder(root);
     
     printf("\n");
-
     return 0;
 }
