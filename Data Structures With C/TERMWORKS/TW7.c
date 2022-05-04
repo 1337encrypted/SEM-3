b22c452f1e7b1b2729aa26871b9dc53489ef7eef
= <=>
2022-05-04 18:18:43 +0530

    Changes to be committed: 	modified:   Data Structures With C/TERMWORKS/TW7.c

--- Data Structures With C/TERMWORKS/TW7.c
+++ Data Structures With C/TERMWORKS/TW7.c
@@ -5,117 +5,105 @@
 
 #include<stdio.h>
 
- /* declare structure for polynomial */
-
 typedef struct poly
 {
-    int coeff;
+    int coff;
     int expo;
 }poly;
 
+int readPoly(poly [30]);
+int addPoly(poly [30], poly [30], poly [30], int, int);
+void display(poly [30], int);
 
- /* function prototypes */
- int readPoly(poly []);
- int addPoly(poly [],poly [],int ,int ,poly []);
- void displayPoly( poly [],int terms);
-
- int main()
- {
-	int t1,t2,t3;
-	poly p1[10],p2[10],p3[10];
-
-	
-	t1=readPoly(p1);
-	printf(" \n First polynomial : ");
-	displayPoly(p1,t1);
-	
-	t2=readPoly(p2);
-	printf(" \n Second polynomial : ");
-	displayPoly(p2,t2);
-
-	
-	t3=addPoly(p1,p2,t1,t2,p3);
-	printf(" \n\n Resultant polynomial after addition : ");
-	displayPoly(p3,t3);
-	printf("\n");
-
-	return 0;
- }
-
- int readPoly(poly p[10])
- {
-	int t1,i;
-	printf("\n\n Enter the total number of terms in the polynomial:");
-	scanf("%d",&t1);
-	printf("\n Enter the COEFFICIENT and EXPONENT in DESCENDING ORDER\n");
-	for(i=0;i<t1;i++)
-    {
-		printf("\nEnter the Coefficient(%d): ",i+1);
-		scanf("%d",&p[i].coeff);
-		printf("\nEnter the exponent(%d): ",i+1);
-		scanf("%d",&p[i].expo);        /* only statement in loop */
-	}
-	return(t1);
- }
-
- int addPoly(poly p1[10],poly p2[10],int t1,int t2,poly p3[10])
- {
-	int i,j,k;
-
-	i=j=k=0;
-
-	while(i<t1 && j<t2)
-	{
-		if(p1[i].expo==p2[j].expo)
-		{
-			p3[k].coeff=p1[i].coeff + p2[j].coeff;
-			p3[k].expo=p1[i].expo;
-
-			i++;
-			j++;
-			k++;
-		}
-		else if(p1[i].expo>p2[j].expo)
-		{
-			p3[k].coeff=p1[i].coeff;
-			p3[k].expo=p1[i].expo;
-			i++;
-			k++;
-		}
-		else
-		{
-			p3[k].coeff=p2[j].coeff;
-			p3[k].expo=p2[j].expo;
-			j++;
-			k++;
-		}
-	}
-
-	
-	while(i<t1)
-	{
-		p3[k].coeff=p1[i].coeff;
-		p3[k].expo=p1[i].expo;
-		i++;
-		k++;
-	}
-	
-	while(j<t2)
-	{
-		p3[k].coeff=p2[j].coeff;
-		p3[k].expo=p2[j].expo;
-		j++;
-		k++;
-	}
-
-	return(k); 
- }
-
- void displayPoly(poly p[10],int term)
- {
-	int k;
-	for(k=0;k<term-1;k++)
-        printf("%d(x^%d)+",p[k].coeff,p[k].expo);
-	printf("%d(x^%d)",p[term-1].coeff,p[term-1].expo);
+int readPoly(poly p[30])
+{
+   int t,i;
+   printf("\nEnter the total number of terms in the polynomial: ");
+   scanf("%d",&t);
+   printf("\nEnter the COEFFICIENT and EXPONENT in DESCENDING ORDER");
+   for(i=0;i<t;i++)
+   {
+       printf("\nEnter the coefficient(%d): ",i+1);
+       scanf("%d",&p[i].coff);
+       printf("\nEnter the exponent(%d): ",i+1);
+       scanf("%d",&p[i].expo);        /* only statement in loop */
+   }
+   return(t);
 }
 
+int addPoly(poly p1[30], poly p2[30], poly p3[30], int t1, int t2)
+{
+    int i,j,k;
+    i=j=k=0;
+    
+    while(i<t1 && j<t2)
+    {
+        if(p1[i].expo == p2[j].expo)
+        {
+            p3[k].coff = p1[i].coff + p2[j].coff;
+            p3[k].expo = p1[i].expo;
+            i++;
+            j++;
+            k++;
+        }
+        else if(p1[i].expo > p2[j].expo)
+        {
+            p3[k].coff = p1[i].coff;
+            p3[k].expo = p1[i].expo;
+            i++;
+            k++;
+        }
+        else
+        {
+            p3[k].coff = p2[j].coff;
+            p3[k].expo = p2[j].expo;
+            j++;
+            k++;
+        }
+    }
+    
+    while(i < t1)
+    {
+        p3[k].coff = p1[i].coff;
+        p3[k].expo = p1[i].expo;
+        i++;
+        k++;
+    }
+    while(j < t2)
+    {
+        p3[k].coff = p2[j].coff;
+        p3[k].expo = p2[j].expo;
+        j++;
+        k++;
+    }
+    return (k);
+}
+
+void display(poly p[30], int t)
+{
+    int i;
+    for(i=0;i<t;i++)
+        printf("%d(x^%d) ",p[i].coff,p[i].expo);
+    printf("\n");
+}
+
+int main(void)
+{
+    poly p1[30], p2[30], p3[30];
+    int t1,t2,t3;
+    
+    t1 = readPoly(p1);
+    printf("\nFirst polynomial: ");
+    display(p1,t1);
+    
+    t2 = readPoly(p2);
+    printf("\nSecond polynomial: ");
+    display(p2,t2);
+    
+    t3 = addPoly(p1,p2,p3,t1,t2);
+    printf("\nSummation of polynomials: ");
+    display(p3,t3);
+    
+    printf("\n");
+    return 0;
+}
