c6e8f0da0ead8b06e5f05ff724c7732167c46d88
= <=>
2022-08-04 22:58:04 +0530

    removed DS_store

--- Java/arrays/isSortedArray.java
+++ Java/arrays/isSortedArray.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
 
+import java.util.ArrayList;
 class isSortedArray{
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<Integer>(args.length);
